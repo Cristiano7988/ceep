c8ac08a9ea5953c4a6fc9d7b507802767167b018
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-06 16:27:21 -0300

    Criação da função de criação de titulo

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,10 +2,25 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 class FormularioCadastro extends Component {
+  constructor() {
+    super();
+    this.titulo = "";
+  }
+
+  criarTitulo(e) {
+    this.titulo = e.target.value;
+    console.log(this.titulo);
+  }
+
   render() {
     return (
       <form className="formulario-cadastro">
-        <input className="input-titulo" type="text" placeholder="Titulo" />
+        <input
+          className="input-titulo"
+          type="text"
+          placeholder="Titulo"
+          onChange={this.criarTitulo.bind(this)}
+        />
         <textarea
           rows={15}
           className="input-nota"
