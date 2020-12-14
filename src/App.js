3137d3285848bfc485f6edeefb4314744f19d6a3
cristiano7988 <cristianooliveiramorales@gmail.com>
2020-12-14 17:14:41 -0300

    Adiciona uma nova categoria dinamicamente

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
+import ListaCategorias from "./components/ListaCategorias";
 import { Component } from "react";
 
 class App extends Component {
   constructor() {
     super();
-    this.notas = [];
     this.state = {
       notas: [],
+      categorias: [],
     };
   }
   criarNota(titulo, texto) {
@@ -25,14 +26,26 @@ class App extends Component {
     this.setState({ notas: ArrayNotas });
   }
 
+  adicionarCategoria(categoria) {
+    const novoArray = [...this.state.categorias, categoria];
+    const novoEstado = {...this.state, categorias: novoArray};
+    this.setState( novoEstado );
+  }
+
   render() {
     return (
       <section className="main-section">
         <FormularioCadastro criarNota={this.criarNota.bind(this)} />
-        <ListaDeNotas
-          notas={this.state.notas}
-          apagarNota={this.apagarNota.bind(this)}
-        />
+        <main>
+          <ListaCategorias
+            categorias={this.state.categorias}
+            adicionarCategoria={this.adicionarCategoria.bind(this)}
+          />
+          <ListaDeNotas
+            notas={this.state.notas}
+            apagarNota={this.apagarNota.bind(this)}
+          />
+        </main>
       </section>
     );
   }
